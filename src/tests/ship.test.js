96292a6000b862eb2202ec78e7b02a567b603a28
Clara Smyth <clarasmyth.work@gmail.com>
2022-09-01 21:48:52 +0100

    Changed filename

---
+++ src/tests/ship.test.js
@@ -0,0 +1,31 @@
+import ship from "../modules/ship";
+
+describe('Ship', () => {
+    test('Ship factory returns an object with length, cells, hit() and isSunk()', () => {
+        expect(ship(3)).toStrictEqual({
+            length: 3,
+            cells: [false, false, false],
+            hit: expect.any(Function),
+            isSunk: expect.any(Function),
+        });
+    })
+    
+    test('Ship hit() modifies the cell of input number', () => {
+        const newShip = ship(3);
+        newShip.hit(1);
+        expect(newShip.cells).toStrictEqual([false, true, false]);
+    })
+    
+    test('Ship isSunk() returns false if all cells are not hit', () => {
+        const newShip = ship(2);
+        newShip.hit(0);
+        expect(newShip.isSunk()).toBe(false);
+    })
+    
+    test('Ship isSunk() returns true if all cells are hit', () => {
+        const newShip = ship(2);
+        newShip.hit(0);
+        newShip.hit(1);
+        expect(newShip.isSunk()).toBe(true);
+    })
+})

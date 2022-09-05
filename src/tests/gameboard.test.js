bee05373d6663f6d793530a69d63bdad9f12f132
Clara Smyth <clarasmyth.work@gmail.com>
2022-09-05 15:19:11 +0100

    Added tests for checkWin function

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -4,6 +4,7 @@ describe('Gameboard', () => {
     test('Gameboard factory returns an object with receiveAttack()', () => {
         expect(gameBoard([[[5, 2], [5, 3]]])).toStrictEqual({
             receiveAttack: expect.any(Function),
+            checkWin: expect.any(Function)
         })
     })
 
@@ -17,5 +18,22 @@ describe('Gameboard', () => {
         let board = gameBoard([[[5, 2], [5, 3]]])
 
         expect(board.receiveAttack([5, 4])).toBe(false);
+    })
+
+    test('Gameboard checkWin() returns true if all ships are sunk', () => {
+        let board = gameBoard([[[5, 2], [5, 3]], [[7, 2], [7, 3]]])
+
+        board.receiveAttack([5, 2])
+        board.receiveAttack([5, 3])
+        board.receiveAttack([7, 2])
+        board.receiveAttack([7, 3])
+
+        expect(board.checkWin()).toBe(true);
+    })
+
+    test('Gameboard checkWin() returns false if all ships are not sunk', () => {
+        let board = gameBoard([[[5, 2], [5, 3]]])
+
+        expect(board.checkWin()).toBe(false);
     })
 })

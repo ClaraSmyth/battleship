dd53254ec72c1d50c0e7b3f944c5ac614e798cdb
Clara Smyth <clarasmyth.work@gmail.com>
2022-09-10 23:17:28 +0100

    Added additional tests

--- src/tests/player.test.js
+++ src/tests/player.test.js
@@ -7,4 +7,16 @@ describe('Player', () => {
             attack: expect.any(Function),
         });
     })
+
+    test('Player can attack enemy player board', () => {
+        const playerOne = player();
+        const playerTwo = player();
+
+        playerOne.attack(playerTwo.board, [5, 2])
+        playerOne.attack(playerTwo.board, [5, 3])
+        playerOne.attack(playerTwo.board, [7, 2])
+        playerOne.attack(playerTwo.board, [7, 3])
+
+        expect(playerTwo.board.checkWin()).toBe(true);
+    })
 })

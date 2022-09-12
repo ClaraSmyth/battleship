c4248511042a51933eb5190bfa6b77f7c4d3a110
Clara Smyth <clarasmyth.work@gmail.com>
2022-09-12 15:44:26 +0100

    Added rough html page structure

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import './styles/styles.scss';
 import gameLoop from './modules/gameloop';
 
+const boardOne = document.querySelector('.board-one');
+const boardTwo = document.querySelector('.board-two');
+
+for (let i = 0; i < 100; i++) {
+    const div = document.createElement('div');
+    const div2 = document.createElement('div');
+    boardOne.append(div);
+    boardTwo.append(div2);
+}
+
 gameLoop();

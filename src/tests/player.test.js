bc419f9df856a3b508009598142473b8cbb472e8
Clara Smyth <clarasmyth.work@gmail.com>
2022-09-12 06:43:21 +0100

    Updated player tests

--- src/tests/player.test.js
+++ src/tests/player.test.js
@@ -4,13 +4,14 @@ describe('Player', () => {
     test('Player factory returns an object with board and attack()', () => {
         expect(player()).toStrictEqual({
             board: expect.any(Object),
+            name: expect.any(String),
             attack: expect.any(Function),
         });
     })
 
     test('Player can attack enemy player board', () => {
-        const playerOne = player([[[5, 2], [5, 3]], [[7, 2], [7, 3]]]);
-        const playerTwo = player([[[5, 2], [5, 3]], [[7, 2], [7, 3]]]);
+        const playerOne = player('Name', [[[5, 2], [5, 3]], [[7, 2], [7, 3]]]);
+        const playerTwo = player('Name', [[[5, 2], [5, 3]], [[7, 2], [7, 3]]]);
 
         playerOne.attack(playerTwo.board, [5, 2])
         playerOne.attack(playerTwo.board, [5, 3])

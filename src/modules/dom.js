3fb9e9cd6d6fffa34b7cc51ebc54ef617de88b41
Clara Smyth <clarasmyth.work@gmail.com>
2022-09-13 21:51:03 +0100

    Added boardController function to take user input

---
+++ src/modules/dom.js
@@ -0,0 +1,28 @@
+const buildBoards = () => {
+    const boardOne = document.querySelector('.board-one');
+    const boardTwo = document.querySelector('.board-two');
+
+    for (let i = 9; i >= 0; i--) {
+        for (let j = 0; j <= 9; j++) {
+            const div = document.createElement('div');
+            const div2 = document.createElement('div');
+
+            div.setAttribute('data-coords', `${i},${j}`);
+            div2.setAttribute('data-coords', `${i},${j}`);
+
+            boardOne.append(div);
+            boardTwo.append(div2);
+        }
+    }
+};
+
+const boardController = (gameloop) => {
+    const boardTwo = document.querySelector('.board-two');
+
+    boardTwo.addEventListener('click', (e) => {
+        const coords = e.target.dataset.coords.split(',').map(Number);
+        gameloop.takeTurn(coords);
+    });
+};
+
+export { buildBoards, boardController };

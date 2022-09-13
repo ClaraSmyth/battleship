8c8cc655791819784f2bc707dd9ccd3090c6dba8
Clara Smyth <clarasmyth.work@gmail.com>
2022-09-13 21:51:15 +0100

    Updated index

--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
 import './styles/styles.scss';
 import gameLoop from './modules/gameloop';
+import { boardController, buildBoards } from './modules/dom';
 
-const boardOne = document.querySelector('.board-one');
-const boardTwo = document.querySelector('.board-two');
-
-for (let i = 0; i < 100; i++) {
-    const div = document.createElement('div');
-    const div2 = document.createElement('div');
-    boardOne.append(div);
-    boardTwo.append(div2);
-}
-
-gameLoop();
+buildBoards();
+const currentGameloop = gameLoop();
+boardController(currentGameloop);

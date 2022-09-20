e16f863b8df706903f0871a80eb2c232f945f218
Clara Smyth <clarasmyth.work@gmail.com>
2022-09-20 16:52:06 +0100

    Updated updateBoard function to display sunk ships

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -28,11 +28,11 @@ const boardController = (gameloop) => {
     });
 };
 
-const updateBoard = (coords, attack, player) => {
+const updateBoard = (coords, attack, player, enemy) => {
     const boardOne = document.querySelector('.board-one');
     const boardTwo = document.querySelector('.board-two');
 
-    const board = player ? boardTwo : boardOne;
+    const board = player.name ? boardTwo : boardOne;
 
     for (let i = 0; i < board.childNodes.length; i++) {
         const node = board.childNodes[i];
@@ -41,6 +41,23 @@ const updateBoard = (coords, attack, player) => {
             node.classList.add(attack ? 'hit' : 'miss');
             break;
         }
+    }
+
+    const sunkShips = enemy.board.checkSunk();
+
+    if (sunkShips) {
+        sunkShips.forEach((ship) => {
+            ship.coords.forEach((coord) => {
+                for (let i = 0; i < board.childNodes.length; i++) {
+                    const node = board.childNodes[i];
+
+                    if (node.dataset.coords === coord.toString()) {
+                        node.classList.add('sunk');
+                        break;
+                    }
+                }
+            });
+        });
     }
 };
 

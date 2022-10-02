4ae15b1aa7d3560d0665e4237abd0cd65a2b40e4
Clara Smyth <clarasmyth.work@gmail.com>
2022-10-02 14:31:01 +0100

    Updated webpack dev config

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,8 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
+
 
 module.exports = {
     mode: 'development',

355ad37c8bfbe540413e3a7bb1ebe1c7578f5472
Clara Smyth <clarasmyth.work@gmail.com>
2022-10-02 15:03:40 +0100

    Updated Index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import './styles/styles.scss';
-import gameLoop from './modules/gameloop';
-import { boardController, buildBoards, startGameModal } from './modules/dom';
+import { buildBoards, startGameModal } from './modules/dom';
 
 buildBoards();
 startGameModal();
-const currentGameloop = gameLoop();
-boardController(currentGameloop);

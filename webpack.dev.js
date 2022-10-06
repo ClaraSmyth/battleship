2bd14753197eda7f28e209fa3bf8f735493fad1e
Clara Smyth <clarasmyth.work@gmail.com>
2022-10-06 07:23:38 +0100

    Updated webpack config

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 
 module.exports = {
@@ -41,6 +42,9 @@ module.exports = {
         }),
         new MiniCssExtractPlugin({
             filename: './[name].[contenthash].css',
+        }),
+        new CopyPlugin({
+            patterns: [{ from: 'src/assets', to: 'assets' }],
         }),
     ],
     optimization: {
